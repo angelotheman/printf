5e2bb19f9e4edce01279eda6962d6402652654bf
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-20 20:37:54 +0000

    Printf like command

---
+++ 0-printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _printf - Print like a printf
+  * @format: Args to the function
+  *
+  * Return: A format string to output
+  */
+
+int _printf(const char format, ...)
+{
+	return (0);
+}
