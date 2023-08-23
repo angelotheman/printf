7a570c7cc6d9b3a0872d4cbef6d5d7490469bcb7
root <root@Bennett>
2023-08-23 10:49:25 +0000

    commit to checkout

--- 1-printf.c
+++ 1-printf.c
@@ -10,8 +10,9 @@ int _printf(const char *format, ...)
 {
     int count = 0;
     va_list args;
-    va_start(args,format);
-
+    
+    va_start(args, format);
+    
     while (*format)
     {
         if (*format == '%')
