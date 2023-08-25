5a1f57146943403a111f4e7fadd4d29df5aa9854
Kwabena Twumasi <ktwum721@gmail.com>
2023-08-25 15:06:49 +0000

    Printf updated

--- 1-printf.c
+++ 1-printf.c
@@ -4,39 +4,40 @@
  * _printf - prints characters to the standard output
  * @format: va variable for arguments
  * Return: total number of characters printed
-*/
+ */
 
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
-    va_start(args,format);
+	int count = 0;
+	va_list args;
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == '\0')
-            break;
-
-            if (*format == 'd' || *format == 'i')
-            {
-                count += print_int(args);
-            }
-            else if (*format == '%')
-            {
-                _putchar('%');
-                count++;
-            }
-        }
-        else
-        {
-            _putchar(*format);
-            count++;
-        }
-        format++;
-    }
-    va_end(args);
-    return (count);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			while (*(format + 1) == ' ')
+				format++;
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == 'd' || *format == 'i')
+			{
+				count += print_int(args);
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
